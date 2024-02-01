fc3712f9c902e657c57ea6f900d2c87828c340d2
drrefactor <drrefactor@gmail.com>
2024-02-01 13:18:32 +0100

    Isolate leak with sharedValue updates

--- App.tsx
+++ App.tsx
@@ -9,50 +9,22 @@
 
 import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
-import Animated, {
-  useAnimatedStyle,
-  useDerivedValue,
-  useSharedValue,
-} from 'react-native-reanimated';
+import {useSharedValue} from 'react-native-reanimated';
 
 function App(): React.JSX.Element {
   const x = useSharedValue(0);
-  const x2 = useSharedValue(0);
-  const x3 = useSharedValue(0);
-  const x4 = useSharedValue(0);
 
   const [s, setS] = useState(0);
 
   useEffect(() => {
-    setInterval(
-      () =>
-        setS(ss => {
-          x.value = ss + 1;
-          return ss + 1;
-        }),
-      50,
-    );
+    setInterval(() => setS(ss => ss + 1), 50);
   }, [x]);
 
-  const y = useDerivedValue(() => s, [s]);
+  useEffect(() => {
+    x.value = s;
+  }, [s, x]);
 
-  const style = useAnimatedStyle(() => {
-    return {
-      transform: [{translateX: x.value % 100}, {translateY: y.value % 100}],
-      backgroundColor: 'blue',
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: 50,
-      height: 50,
-    };
-  }, [x, y]);
-
-  return (
-    <View style={{flex: 1, backgroundColor: 'red'}}>
-      <Animated.View style={style} />
-    </View>
-  );
+  return <View style={{flex: 1, backgroundColor: 'red'}} />;
 }
 
 export default App;
